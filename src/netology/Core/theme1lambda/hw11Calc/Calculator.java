2bee037c992e266caa115ce8e8572db6bdd01932
Smile78 <dronussr@gmail.com>
2022-02-22 21:42:01 +0300

    calc exception

--- src/netology/Core/theme1lambda/hw11Calc/Calculator.java
+++ src/netology/Core/theme1lambda/hw11Calc/Calculator.java
@@ -1,5 +1,6 @@
 package netology.Core.theme1lambda.hw11Calc;
 
+import java.io.IOException;
 import java.util.function.*;
 
 public class Calculator {
@@ -9,7 +10,24 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+
+
+    BinaryOperator<Integer> devide = (x, y) -> {
+        int z =0;
+        try {
+            z= x/y;
+         } catch (ArithmeticException exc){
+            System.out.println("Йохохоу АрифметикException....\n" +
+                    "нельзя делить на 0 и похорошему тут наверно надо все это заново вводить ... \n" +
+                    "но какбы в задании такого нет... \n" );
+        } finally {
+            System.out.println(
+                    "такчто...\n" +
+                            "Файнали ...\n" +
+                            " у=1 (ну какбы b) и какбы Thats All Folks!\n ");
+            if (y==0)  z= x/1;
+        }
+        return z;  };
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
